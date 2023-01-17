c7d9dda13fecddb2d7374e5901c83b60fc00d15b
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-17 17:16:17 +0900

    MemberMapStore 구현 완료

--- travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/service/logic/MemberServiceLogic.java
+++ travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/service/logic/MemberServiceLogic.java
@@ -4,12 +4,18 @@ import io.namoosori.travelclub.spring.aggregate.club.CommunityMember;
 import io.namoosori.travelclub.spring.service.MemberService;
 import io.namoosori.travelclub.spring.service.sdo.MemberCdo;
 import io.namoosori.travelclub.spring.shared.NameValueList;
+import io.namoosori.travelclub.spring.store.MemberStore;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MemberServiceLogic implements MemberService {
+
+    private final MemberStore memberStore;
+
     @Override
     public String registerMember(MemberCdo member) {
         return null;
